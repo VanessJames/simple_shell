ff50aa22c96b133791f77a6b52736a4747297d4a
VanessJames <vanessjames10@gmail.com>
2023-07-26 13:27:36 -0700

    Back to the Basics

--- shell.c
+++ shell.c
@@ -1,156 +1,95 @@
 #include "shell.h"
-
-static char buffer[BUFFER_SIZE];
-static int buffer_pos;
-static int buffer_size;
+#include <sys/wait.h>
+#include <unistd.h>
 
 /**
- * custom_getline - Read a line from standard input.
- * @line: The buffer to store the read line.
- * @size: The size of the buffer.
- *
- * Description: This function reads a line from standard
- * input using a buffer.It calls the least possible the
- * read system call to fill the buffer.
- *
- * Return: The number of characters read (including the newline character),
- * -1 on failure or when encountering the end of file (Ctrl+D).
+ * display_prompt - Displays the shell prompt.
  */
-static ssize_t custom_getline(char *line, size_t size)
+
+void display_prompt(void)
 {
-	ssize_t read_bytes = 0;
-	int i;
-
-	while (1)
-	{
-		if (buffer_pos == buffer_size)
-		{
-			/* Buffer is empty, read from standard input*/
-			buffer_size = read(STDIN_FILENO, buffer, BUFFER_SIZE);
-			if (buffer_size <= 0)
-				/* Error or end of file (Ctrl+D)*/
-				return (-1);
-			buffer_pos = 0;
-		}
-
-		for (i = buffer_pos; i < buffer_size &&
-				read_bytes < (ssize_t)(size - 1); i++)
-		{
-			if (buffer[i] == '\n')
-			{
-				/* Found newline, include it in the line and terminate*/
-				line[read_bytes] = '\n';
-				line[read_bytes + 1] = '\0';
-				buffer_pos = i + 1;
-				return (read_bytes + 1);
-			}
-			else
-			{
-				line[read_bytes++] = buffer[i];
-			}
-		}
-
-		buffer_pos = buffer_size;
-	}
-
-	line[read_bytes] = '\0';
-	return (read_bytes);
+	printf("($) ");
 }
 
 /**
- * process_command - Process and execute a command.
- * @input: The user input string containing the command.
+ * execute_command - Executes the given command with arguments.
+ * @args: The array of pointers to arguments (command and its arguments).
  *
- * Description: This function takes the user input string, tokenizes it, and
- * executes the appropriate command based on the input provided.
+ * Return: 1 on success, -1 on error.
  */
 
-static void process_command(char *input)
+int execute_command(char **args)
 {
-	char *tokens[MAX_TOKENS];
+	pid_t pid, wpid;
+	int status;
 
-	parse_input(input, tokens);
-
-	if (tokens[0] != NULL)
+	/* Fork a new process*/
+	pid = fork();
+	if (pid == -1)
 	{
-		/* Handle exit built-in command */
-		if (strcmp(tokens[0], "exit") == 0)
+		perror("fork");
+		return (-1);
+	}
+	else if (pid == 0)
+	{
+		/* Child process*/
+
+		if (execve(args[0], args, environ) == -1)
 		{
-			if (tokens[1] != NULL)
-			{
-				handle_exit(tokens[1]);
-			}
-			else
-			{
-				handle_exit(NULL);
-			}
-		}
-		/* Handle env built-in command */
-		else if (strcmp(tokens[0], "env") == 0)
-		{
-			handle_env();
-		}
-		/* Execute other commands */
-		else
-		{
-			if (!execute_command(tokens[0], tokens))
-			{
-				fprintf(stderr, "Command not found: %s\n", tokens[0]);
-			}
+			perror("execve");
+			exit(EXIT_FAILURE);
 		}
 	}
+	else
+	{
+	/* Parent process*/
+
+	do {
+		wpid = waitpid(pid, &status, WUNTRACED);
+		if (wpid == -1)
+		{
+			perror("waitpid");
+			return (-1);
+		}
+		} while (!WIFEXITED(status) && !WIFSIGNALED(status));
+
+	}
+
+	return (1);
 }
 
 /**
- * interactive_shell_loop - Run the interactive shell loop.
- *
- * Description: This function initializes the interactive shell loop,
- * displaying the shell name at startup, and continuously waits for user input.
- * It reads user input using the custom_getline function, parses the
- * input into tokens, and then processes and executes the commands
- * based on the input provided by the user.
+ * execute_command_line - Executes a single command line.
+ * @input: The input command line.
  */
 
-static void interactive_shell_loop(void)
+void execute_command_line(char *input)
 {
-	ssize_t bytes_read;
-	char input[MAX_INPUT_LENGTH];
+	char **args;
+	int status;
 
-	while (1)
+	args = split_input(input);
+	if (args[0] != NULL)
 	{
-		printf("(Vaness&Jessica's Shell) "); /* Display the prompt */
-
-		/* Read user input */
-		bytes_read = custom_getline(input, MAX_INPUT_LENGTH);
-
-		if (bytes_read == -1)
+		if (strcmp(args[0], "exit") == 0)
 		{
-			printf("\n"); /* Handle end of file (Ctrl+D) */
-			break;
+			free_args(args);
+			free(input);
+			exit(EXIT_SUCCESS);
+		}
+		else if (strcmp(args[0], "env") == 0)
+		{
+			print_environment();
+			free_args(args);
+			free(input);
+			return;
 		}
 
-		input[bytes_read - 1] = '\0';
-		process_command(input);
+		status = execute_command(args);
+		if (status == -1)
+			fprintf(stderr, "%s: %s: not found\n", args[0], args[0]);
 	}
-}
 
-/**
- * main - Entry point of the shell program.
- *
- * Description: This function initializes the interactive shell loop,
- * displaying the shell name at startup and continuously
- * waits for user input. It reads user input using the
- * custom_getline function, parses the input into tokens,
- * and then executes the appropriate commands based on the
- * input provided by the user.
- *
- * Return: Always returns 0.
- */
-
-int main(void)
-{
-	printf("Welcome to Vaness&Jessica's Shell!\n");
-	interactive_shell_loop();
-
-	return (0);
+	free_args(args);
+	free(input);
 }
